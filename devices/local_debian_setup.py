d1cdd159d25908c812f7db49b7a435ccefc74b68
Eric Schultz <eschultz@prplfoundation.org>
2016-03-23 13:37:56 -0500

    non-root is necessary

--- devices/local_debian_setup.py
+++ devices/local_debian_setup.py
@@ -24,7 +24,7 @@ class LocalDebianSetup(debian.DebianBox):
         cprint("%s device console = %s" % ("local device", colored(color, color)), None, attrs=['bold'])
         self.expect(self.prompt)
 
-        self.sendline("exec 2> /tmp/run.log")
+        self.sendline("exec 2> /tmp/local_run.log")
         self.sendline("exec 1>&2")
         self.sendline("set -x")
         self.expect(self.prompt)
