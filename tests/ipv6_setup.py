2519259af228ce5072760fe5450e402558b08a86
Eric Schultz <eschultz@prplfoundation.org>
2016-04-08 15:22:07 -0500

    remove unneeded space

--- tests/ipv6_setup.py
+++ tests/ipv6_setup.py
@@ -71,7 +71,7 @@ class Set_IPv6_Addresses(rootfs_boot.RootFSBootTest):
         board.sendline('\nifconfig | grep -B2 addr:')
         board.expect('ifconfig ')
         board.expect(prompt)
-        lan.sendline('\nifconfig  | grep -B2 addr:')
+        lan.sendline('\nifconfig | grep -B2 addr:')
         lan.expect('ifconfig ')
         lan.expect(prompt)
         wan.sendline('\nifconfig | grep -B2 addr:')
