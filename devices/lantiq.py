1456b83433fc0d931c410d0acf500e47434313f7
Eric Schultz <eschultz@prplfoundation.org>
2016-09-29 15:25:27 -0500

    fix super call

--- devices/lantiq.py
+++ devices/lantiq.py
@@ -17,7 +17,7 @@ class LantiqRouter(openwrt_router.OpenWrtRouter):
     uprompt = ['VR9 #']
 
     def __init__(self, *args, **kwargs):
-        super(openwrt_router.OpenWrtRouter, self).__init__(*args, **kwargs)
+        super(LantiqRouter, self).__init__(*args, **kwargs)
 
     def flash_meta(self, META_BUILD):
         common.print_bold("\n===== Flashing image on=====\n")
